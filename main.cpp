23a4644b6bd61cc222289ac9962569aee5bf6308
Arslan Shaukat <eskillspoint@gmail.com>
2022-05-06 09:57:47 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ int main(void)
+{
+	 char *first_string = "this is the first string";
+	 string second_string = "this is the second string";
+	 const string third_string = "this is the third string";
+	 cout << first_string << endl;
+	 return 0;
+}
